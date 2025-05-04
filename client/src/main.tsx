608eded1dc29f7cbc290647997f2c53d635b7cfe
eimaam <imamddahir@gmail.com>
2025-05-04 16:15:23 +0100

    refactor: remove BrowserRouter from main.tsx as routing is handled elsewhere

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './globals.css'
 import { ThemeProvider } from './components/ui/theme-provider'
@@ -8,12 +7,10 @@ import { UserProvider } from './contexts/UserContext'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
       <ThemeProvider>
         <UserProvider>
           <App />
         </UserProvider>
       </ThemeProvider>
-    </BrowserRouter>
   </React.StrictMode>
 )
